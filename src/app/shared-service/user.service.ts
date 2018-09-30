701159a97794deb0e5d4f945615cbab3a2b19c6c
Nalin Kumar <38244351+Nikhil12894@users.noreply.github.com>
2018-09-30 07:57:22 +0530

    Update user.service.ts

--- src/app/shared-service/user.service.ts
+++ src/app/shared-service/user.service.ts
@@ -10,7 +10,7 @@ import{User}  from '../user';
 
 @Injectable()
 export class UserService {
-  private baseUrl:string='http://localhost:8080/api';
+  private baseUrl:string='http://localhost:8080/RestDemo/rest/test';
   private headers = new Headers({'Content-Type':'application/json'});
   private options = new RequestOptions({headers:this.headers});
   private user = new User();
@@ -18,30 +18,30 @@ export class UserService {
 
   getUsers(){
 
-    return this._http.get(this.baseUrl+'/users',this.options).map((response:Response)=>response.json())
+    return this._http.get(this.baseUrl+'/students',this.options).map((response:Response)=>response.json())
       .catch(this.errorHandler);
   }
   getUser(id:Number){
 
-    return this._http.get(this.baseUrl+'/user/'+id,this.options).map((response:Response)=>response.json())
+    return this._http.get(this.baseUrl+'/student/'+id,this.options).map((response:Response)=>response.json())
       .catch(this.errorHandler);
   }
   deleteUser(id:Number){
 
-    return this._http.delete(this.baseUrl+'/user/'+id,this.options).map((response:Response)=>response.json())
+    return this._http.delete(this.baseUrl+'/delete/'+id,this.options).map((response:Response)=>response.json())
       .catch(this.errorHandler);
   }
 
 
   createUser(user:User){
 
-    return this._http.post(this.baseUrl+'/user',JSON.stringify(user),  this.options).map((response:Response)=>response.json())
+    return this._http.post(this.baseUrl+'/add',JSON.stringify(user),  this.options).map((response:Response)=>response.json())
       .catch(this.errorHandler);
   }
    
    updateUser(user:User){
 
-    return this._http.put(this.baseUrl+'/user',JSON.stringify(user),  this.options).map((response:Response)=>response.json())
+    return this._http.put(this.baseUrl+'/put',JSON.stringify(user),  this.options).map((response:Response)=>response.json())
       .catch(this.errorHandler);
   }
   
